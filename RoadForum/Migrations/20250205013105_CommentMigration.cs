c88cb38e03a0020dd0a5797b162afa3299026a01
Ryan Morris <ryanmorris971@gmail.com>
2025-02-04 21:45:16 -0400

    reran comment migration.

---
+++ RoadForum/Migrations/20250205013105_CommentMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoadForum.Migrations
+{
+    /// <inheritdoc />
+    public partial class CommentMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
