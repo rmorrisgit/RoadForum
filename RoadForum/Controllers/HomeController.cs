311442ff3e37973e892498d4d5687431df06e62d
Ryan Morris <ryanmorris971@gmail.com>
2025-02-05 21:50:47 -0400

    displsaying discussions on homepage

--- RoadForum/Controllers/HomeController.cs
+++ RoadForum/Controllers/HomeController.cs
@@ -1,33 +1,35 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RoadForum.Data; // Assuming this is your DbContext namespace
 using RoadForum.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace RoadForum.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            // Create a hardcoded list of discussions
-            List<Discussion> discussions = new List<Discussion>
-            {
+        private readonly RoadForumContext _context;
 
-            };
+        public HomeController(RoadForumContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // Fetch discussions from the database
+            var discussions = await _context.Discussion.ToListAsync();
 
             // Pass the list to the view
             return View(discussions);
         }
 
-        public IActionResult DiscussionDetails(int id)
+        public async Task<IActionResult> DiscussionDetails(int id)
         {
-            // Simulate fetching a discussion by ID
-            var discussions = new List<Discussion>
-            {
-  
-            };
-
-            // Find the discussion with the matching ID
-            var discussion = discussions.Find(d => d.DiscussionId == id);
+            // Fetch the discussion by ID from the database
+            var discussion = await _context.Discussion.FirstOrDefaultAsync(d => d.DiscussionId == id);
 
             if (discussion == null)
             {
