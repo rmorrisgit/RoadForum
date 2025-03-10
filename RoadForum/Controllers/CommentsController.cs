73e2b47d9b9880c64aab0427c6883974b0390d2f
rmorrisgit <ryanmorris971@gmail.com>
2025-03-10 00:45:21 -0300

    start of protecting routes

--- RoadForum/Controllers/CommentsController.cs
+++ RoadForum/Controllers/CommentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,8 @@ using RoadForum.Models;
 
 namespace RoadForum.Controllers
 {
+    // only logged in users have access
+    [Authorize]
     public class CommentsController : Controller
     {
         private readonly RoadForumContext _context;
